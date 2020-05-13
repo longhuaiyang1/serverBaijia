963dbcf4afcadaae179d300db362750be935d9ba
longhuaiyang <545482979@qq.com>
2020-05-13 18:31:26 +0800

    json解析问题

--- src/main/java/com/baijia/lhy/controller/UserOrderController.java
+++ src/main/java/com/baijia/lhy/controller/UserOrderController.java
@@ -4,8 +4,8 @@ package com.baijia.lhy.controller;
 import com.baijia.lhy.pojo.dto.Result;
 import com.baijia.lhy.pojo.entity.UserOrder;
 import com.baijia.lhy.service.IUserOrderService;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import net.minidev.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +18,7 @@ import java.util.LinkedHashMap;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author lhy
@@ -30,38 +30,39 @@ public class UserOrderController {
     IUserOrderService userOrderService;
 
     @PostMapping("/uploadOrder")
-    public Result getReceivePoints(@RequestBody JSONObject jsonObject) {
-        try{
+    public Result getReceivePoints(@RequestBody String str) {//这里可以用net.minidev.json.JSONObject直接入参，但不能用org.json.JSONObject
+        try {
+            JSONObject jsonObject = new JSONObject(str);
             UserOrder userOrder = new UserOrder();
-            userOrder.setUserId(1);////////////////////////////////
+            ////////////////////////////////////////
+            userOrder.setUserId(1);
 
-            LinkedHashMap receivePoint = (LinkedHashMap) jsonObject.get("receivePoint");
-            userOrder.setReceivePointId((Integer) receivePoint.get("id"));
+            JSONObject receivePoint = jsonObject.getJSONObject("receivePoint");
+            userOrder.setReceivePointId(receivePoint.getInt("id"));
 
-            userOrder.setReceiverName(jsonObject.get("receiverUserName").toString());
-            userOrder.setReceiverPhone( jsonObject.get("receiverUserPhone").toString());
+            userOrder.setReceiverName(jsonObject.getString("receiverUserName"));
+            userOrder.setReceiverPhone(jsonObject.getString("receiverUserPhone"));
+            userOrder.setProducts(jsonObject.getJSONArray("productsArray").toString());
+            userOrder.setEstimateTotalCost(Double.valueOf(jsonObject.getString("totalPrice")));
+
             userOrder.setPayType("cash");
-            userOrder.setProducts(jsonObject.getAsString("productsArray"));
-            userOrder.setEstimateTotalCost(Double.valueOf(jsonObject.getAsString("totalPrice")));
-//
             userOrder.setStatus(0);//0待支付
 
             LocalDateTime time = LocalDateTime.now(Clock.system(ZoneId.of("Asia/Shanghai")));
             userOrder.setCreateTime(time);
             userOrder.setUpdateTime(time);
 
-            boolean success= userOrderService.save(userOrder);
+            boolean success = userOrderService.save(userOrder);
 
-            System.out.println("1111111"+jsonObject);
             Result result = new Result();
-            if(success){
+            if (success) {
                 result.setCode(2001);
                 result.setMsg("");
-                result.setData(new JSONObject());
-                return  result;
+                result.setData(new net.minidev.json.JSONObject());//这里用org.json.JSONObject会报错，不知道为啥？序列化傻的报错
+                return result;
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -69,8 +70,8 @@ public class UserOrderController {
         Result result = new Result();
         result.setCode(3006);
         result.setMsg("失败");
-        result.setData(new JSONObject());
-        return  result;
+        result.setData(new net.minidev.json.JSONObject());
+        return result;
     }
 }
 
--- src/main/java/com/baijia/lhy/pojo/dto/Result.java
+++ src/main/java/com/baijia/lhy/pojo/dto/Result.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-public class Result {
+public class Result implements Serializable {
     int code; //200~300之间表示正确，否则表示错误
     String msg;
     Object data;
